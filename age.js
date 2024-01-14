d6a75be380f751c2b1857ec0f05eb097a0714afb
Dmytro Arkhipov <arkhipovdmytro@gmail.com>
2024-01-14 17:00:55 +0200

    changed age.js

--- age.js
+++ age.js
@@ -2,9 +2,17 @@ let age = prompt("Веедіть ваш вік", "20")
 let message = ''
 
 if(!isNaN(+age) && +age >= 0){
-    message = (((+age/10) % 10) > 1 && ((+age/10) % 10) < 2) ? age + " років" : (+age % 10) === 1 ? age +" рік" : ((+age % 10) < 5 && +age % 10 > 1) ? age + " роки" : age + " років"
+    if (((+age/10) % 10) > 1 && ((+age/10) % 10) < 2){
+        message =  age + " років"
+    } else if ((+age % 10) === 1) {
+        message = age +" рік"
+    } else if ((+age % 10) < 5 && (+age % 10) > 1) {
+        message = age + " роки"
+    } else {
+        message = age + " років"
+    }
 }
 else {
-    message = +age < 0 ? "Від'ємне число не може бути віком" : "Відповіддю має бути числом"
+    message = +age < 0 ? "Від'ємне число не може бути віком" : "Відповіддю має бути число"
 }
 console.log(message)
