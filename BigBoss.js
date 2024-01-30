d7c07f093a3d4abe0ec870964fc5b2283870b9be
Dmytro Arkhipov <arkhipovdmytro@gmail.com>
2024-01-30 23:06:15 +0200

    completed big boss task

---
+++ BigBoss.js
@@ -0,0 +1,64 @@
+const company = {
+    name: 'Велика Компанія',
+    type:'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+        {
+            name: 'Клієнт 1',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+                {
+                    name: 'Клієнт 1.1',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                },
+                {
+                    name: 'Клієнт 1.2',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                    partners: [
+                        {
+                            name: 'Клієнт 1.2.3',
+                            type: 'subSubCompany',
+                            uses: 'Рішення для продажу квитків',
+                            sells: 'Рішення для продажу квитків',
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Клієнт 2',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+                {
+                    name: 'Клієнт 2.1',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                }
+            ]
+        }
+    ]
+};
+
+function findValueByKey(companyName, companiesList=company.clients) {
+    let nestedPartners = []
+    for(let company of companiesList) {
+        if(company.name === companyName) return JSON.stringify(company, null, 4)
+        if (company.partners && Array.isArray(company.partners)){
+            nestedPartners = nestedPartners.concat(company.partners)
+        }
+    }
+    if(nestedPartners.length === 0) return 'No company with such a anme was found'
+    return findValueByKey(companyName, nestedPartners)
+};
+
+console.log(findValueByKey('Клієнт 1.1'))
